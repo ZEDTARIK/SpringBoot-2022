37f49956ae34daf60a51faa860008e92e28aa3df
zouhair <ettarak.zouhair@gmail.com>
2022-03-22 20:21:22 +0100

    Prepare Envirenement Project

---
+++ src/main/java/app/ws/ettarak/controllers/UserController.java
@@ -0,0 +1,12 @@
+package app.ws.ettarak.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users") // sert à taper en url localhost:8080/users 
+public class UserController {
+
+	
+	
+}

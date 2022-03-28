8ed4cf5079fd81732c80aafc6ad4793d9018976d
zouhair <ettarak.zouhair@gmail.com>
2022-03-28 22:50:00 +0000

    implements methode createUser servoce

--- src/main/java/app/ws/ettarak/repositories/UserRepository.java
+++ src/main/java/app/ws/ettarak/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package app.ws.ettarak.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import app.ws.ettarak.entities.UserEntity;
 
+@Repository
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
 
 }
--- src/main/java/app/ws/ettarak/services/impl/UserServiceImpl.java
+++ src/main/java/app/ws/ettarak/services/impl/UserServiceImpl.java
@@ -1,17 +1,35 @@
 package app.ws.ettarak.services.impl;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import app.ws.ettarak.entities.UserEntity;
+import app.ws.ettarak.repositories.UserRepository;
 import app.ws.ettarak.services.UserService;
 import app.ws.ettarak.shared.dto.UserDto;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+	@Autowired
+	UserRepository userRepository;
+
 	@Override
 	public UserDto createUser(UserDto userDto) {
-		// TODO Auto-generated method stub
-		return null;
+
+		UserEntity userEntity = new UserEntity();
+		BeanUtils.copyProperties(userDto, userEntity);
+		
+		userEntity.setEncryptedPassword("Password Encrypted");
+		userEntity.setUserId("user Id");
+
+		UserEntity newUser = userRepository.save(userEntity);
+		UserDto userDto1 = new UserDto();
+
+		BeanUtils.copyProperties(newUser, userDto1);
+
+		return userDto1;
 	}
 
 }

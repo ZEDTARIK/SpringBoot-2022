0df39dfdc67dc29f7c7e9c0fa6b8ce64b64b4749
zouhair <ettarak.zouhair@gmail.com>
2022-03-28 19:28:40 +0000

    Create controller & userRequest

--- src/main/java/app/ws/ettarak/controllers/UserController.java
+++ src/main/java/app/ws/ettarak/controllers/UserController.java
@@ -1,12 +1,38 @@
 package app.ws.ettarak.controllers;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import app.ws.ettarak.requests.UserRequest;
+
 @RestController
-@RequestMapping("/users") // sert à taper en url localhost:8080/users 
+@RequestMapping("/users") 
 public class UserController {
 
+	@GetMapping
+	public String getUsers() {
+		
+		return "get uers was called !";
+	}
 	
+	@PostMapping
+	public String createUser(@RequestBody UserRequest userRequest) {
+		return "Create user was called !";
+	}
+	
+	@PutMapping
+	public String updateUser() {
+		return "update user was called !";
+	}
+	
+	@DeleteMapping
+	public String DeleteUser() {
+		return "Delete user was called";
+	}
 	
 }

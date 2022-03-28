077c2dba5df53e2cc9a4f0aacdbe39cb79bd58e7
zouhair <ettarak.zouhair@gmail.com>
2022-03-28 22:19:14 +0000

    Create Entity User

--- src/main/java/app/ws/ettarak/entities/UserEntity.java
+++ src/main/java/app/ws/ettarak/entities/UserEntity.java
@@ -1,81 +1,110 @@
-package app.ws.ettarak.shared.dto;
+package app.ws.ettarak.entities;
 
 import java.io.Serializable;
 
-public class UserDto implements Serializable {
-	
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+public class UserEntity implements Serializable {
+
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 8573246675910357539L;
+	private static final long serialVersionUID = 1854935883855826874L;
+
+	@Id
+	@GeneratedValue
 	private long id;
+
+	@Column(nullable = false)
 	private String userId;
+
+	@Column(nullable = false, length = 50)
 	private String firstName;
+
+	@Column(nullable = false, length = 50)
 	private String lastName;
+
+	@Column(nullable = false)
 	private String email;
-	private String password;
+
+	@Column(nullable = false)
 	private String encryptedPassword;
+
+	@Column(nullable = true)
 	private String emailVerificationToken;
+
+	@Column(columnDefinition = "Boolean default false")
 	private boolean emailVerificationStatus;
+
 	
-	
+	// SETTERS & GETTERS 
 	
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public String getUserId() {
 		return userId;
 	}
+
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
+
 	public String getEncryptedPassword() {
 		return encryptedPassword;
 	}
+
 	public void setEncryptedPassword(String encryptedPassword) {
 		this.encryptedPassword = encryptedPassword;
 	}
+
 	public String getEmailVerificationToken() {
 		return emailVerificationToken;
 	}
+
 	public void setEmailVerificationToken(String emailVerificationToken) {
 		this.emailVerificationToken = emailVerificationToken;
 	}
-	public Boolean isEmailVerificationStatus() {
+
+	public boolean isEmailVerificationStatus() {
 		return emailVerificationStatus;
 	}
-	public void setEmailVerificationStatus(Boolean emailVerificationStatus) {
+
+	public void setEmailVerificationStatus(boolean emailVerificationStatus) {
 		this.emailVerificationStatus = emailVerificationStatus;
 	}
-	
-	
-	
 
+	
+	
 }
